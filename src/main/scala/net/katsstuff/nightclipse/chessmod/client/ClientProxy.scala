bbc899136f54755c7f58ee0b29b4346ef63f218b
ArekkuusuJerii <edgargcx99@gmail.com>
2018-03-30 18:58:25 -0600

    Register Model Item Variants for each Piece

--- src/main/scala/net/katsstuff/nightclipse/chessmod/client/ClientProxy.scala
+++ src/main/scala/net/katsstuff/nightclipse/chessmod/client/ClientProxy.scala
@@ -2,8 +2,9 @@ package net.katsstuff.nightclipse.chessmod.client
 
 import net.katsstuff.nightclipse.chessmod.item.ItemPiece
 import net.katsstuff.nightclipse.chessmod.{ChessMod, CommonProxy, Piece}
-import net.minecraft.client.renderer.block.model.{ModelResourceLocation => MRL}
+import net.minecraft.client.renderer.block.model.{ModelBakery, ModelResourceLocation => MRL}
 import net.minecraft.item.ItemStack
+import net.minecraft.util.ResourceLocation
 import net.minecraftforge.client.event.ModelRegistryEvent
 import net.minecraftforge.client.model.ModelLoader
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
@@ -12,12 +13,17 @@ object ClientProxy {
 
   @SubscribeEvent
   def registerModels(event: ModelRegistryEvent): Unit = {
-    import net.katsstuff.nightclipse.chessmod.{ChessBlocks => blocks, ChessItems => items}
+    import net.katsstuff.nightclipse.chessmod.{PieceColor, PieceType, ChessItems => items}
 
     ModelLoader.setCustomMeshDefinition(items.Piece, (stack: ItemStack) => {
       val Piece(tpe, color) = ItemPiece.pieceOf(stack)
       new MRL(ChessMod.resource(s"piece/${color.name}_${tpe.name}"), "inventory")
     })
+    val allPieces = for {
+      tpe   <- PieceType.all
+      color <- PieceColor.all
+    } yield new ResourceLocation(ChessMod.Id, s"piece/${color.name}_${tpe.name}")
+    ModelBakery.registerItemVariants(items.Piece, allPieces.toArray: _*)
   }
 }
 class ClientProxy extends CommonProxy {}
